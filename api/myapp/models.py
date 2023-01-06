673d06d0c502e263200c7ace824acb7825fa55c8
ynikhil121 <109204762+ynikhil121@users.noreply.github.com>
2023-01-06 04:27:49 -0500

    changes in model

--- api/myapp/models.py
+++ api/myapp/models.py
@@ -8,34 +8,3 @@ class Article(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     author = models.CharField(max_length=255)
-
-# # myapp/serializers.py
-# from rest_framework import serializers
-# from .models import Article
-
-# class ArticleSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Article
-#         fields = ['id', 'title', 'body', 'author']
-
-# # myapp/views.py
-# from django.shortcuts import render
-# from rest_framework import viewsets
-# from .models import Article
-# from .serializers import ArticleSerializer
-
-# class ArticleViewSet(viewsets.ModelViewSet):
-#     queryset = Article.objects.all()
-#     serializer_class = ArticleSerializer
-
-# # myapp/urls.py
-# from django.urls import path, include
-# from rest_framework import routers
-# from . import views
-
-# router = routers.DefaultRouter()
-# router.register('articles', views.ArticleViewSet)
-
-# urlpatterns = [
-#     path('', include(router.urls)),
-# ]
